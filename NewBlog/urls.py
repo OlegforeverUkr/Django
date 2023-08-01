b555bc8cfa167a1d6f15c442c6ba20bb916920c8
OlegforeverUkr <130793460+OlegforeverUkr@users.noreply.github.com>
2023-08-01 16:32:08 +0300

    views (#1)
    * views
    
    * fix
    
    * last fix

--- NewBlog/urls.py
+++ NewBlog/urls.py
@@ -16,11 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello_view),
+    path('', views.empty_view),
+    path('<article>/comment/', views.article_comment),
+    path('create/', views.article_create_view),
+    path('<article>/update/', views.article_update_view),
+    path('<article>/delete/', views.article_delete_view),
+    path('topics/', views.topics),
+    path('topics/<topic>/subscribe/', views.topics_subscribe),
+    path('topics/<topic>/unsubscribe/', views.topics_unsubscribe),
+    path('profile/<str:username>/', views.profile_user),
+    path('set-password/', views.set_password),
+    path('set-userdata/', views.set_userdata),
+    path('deactivate/', views.deactivate),
+    path('register/', views.register),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('<article>/', views.article_detail_view),
 ]
---
+++ NewBlog/views.py
@@ -0,0 +1,69 @@
+from django.http import HttpRequest, HttpResponse
+
+
+def hello_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Hello Page')
+
+
+def empty_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Empty Page')
+
+
+def article_detail_view(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f'Page from article - {article}')
+
+
+def article_comment(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f'Comment to article - {article}')
+
+
+def article_create_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Create Page')
+
+
+def article_update_view(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f'Update to article - {article}')
+
+
+def article_delete_view(request: HttpRequest, article: str) -> HttpResponse:
+    return HttpResponse(f'Delete to article - {article}')
+
+
+def topics(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page with topics')
+
+
+def topics_subscribe(request: HttpRequest, topic: str) -> HttpResponse:
+    return HttpResponse(f'Subscribe on topic - {topic}')
+
+
+def topics_unsubscribe(request: HttpRequest, topic: str) -> HttpResponse:
+    return HttpResponse(f'Unsubscribe on topic - {topic}')
+
+
+def profile_user(request: HttpRequest, username: str) -> HttpResponse:
+    return HttpResponse(f'Profile - {username}')
+
+
+def set_password(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page set password')
+
+
+def set_userdata(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page userdata')
+
+
+def deactivate(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page deactivate')
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page register')
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page login')
+
+
+def logout(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Page logout')
