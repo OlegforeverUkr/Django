45f9e528c90b3ae2385ac23158429f655a7d70df
Serhii Horodilov <sgorodil@gmail.com>
2023-08-12 18:17:55 +0300

    Fix pg initialization query

--- docker/db/postgres.init.sql
+++ docker/db/postgres.init.sql
@@ -1,8 +1,2 @@
-/* put database initialization script here */
-
--- for example
 CREATE ROLE myuser WITH ENCRYPTED PASSWORD '123' LOGIN;
-COMMENT ON ROLE docker IS 'docker user for tests';
-
 CREATE DATABASE django OWNER myuser;
-COMMENT ON DATABASE docker IS 'docker db for tests owned by docker user';
